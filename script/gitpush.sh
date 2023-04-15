6afbfc135d5dd405f88de3a35dee6750cecfb7a1
Pixeltrace@Printer <twimmer@mailws.de>
2023-04-15 09:28:25 +0200

    Update 20230415-092825

--- script/gitpush.sh
+++ script/gitpush.sh
@@ -1,12 +1,10 @@
 cd /home/pi/klipper_config
 git fetch
-#git checkout master
-#git pull
 git checkout -b patch-from-printer
-#git merge master
 git add -u
 git reset -- .moonraker.conf.bkp
 git commit -m "Update $(date +"%Y%m%d-%H%M%S")"
 git push --set-upstream origin patch-from-printer
+git merge master
 git checkout master
 git branch -d patch-from-printer
