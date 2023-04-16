f36a8a94938a5fbd8708bfbe6f4c19f99504c795
Pixeltrace@Printer <twimmer@mailws.de>
2023-04-16 10:25:55 +0200

    Update 20230416-102555

--- script/cj_run.sh
+++ script/cj_run.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 function check_online
 {
-	netcat -z -w 5 github.com 22 && echo 1 || echo 0
+	if ping -c 1 github.com &> /dev/null; then
+		echo 1;
+	else
+		echo 0;
+	fi
 }
 
 # Initial check to see if we are online
 IS_ONLINE=$(check_online)
 # How many times we should check if we're online - this prevents infinite looping
-MAX_CHECKS=5
+MAX_CHECKS=10
 # Initial starting value for checks
 CHECKS=0
 
 # Loop while we're not online.
-while [ $IS_ONLINE -eq 0 ]; do
+while [[ $IS_ONLINE -eq 0 ]]; do
     # We're offline. Sleep for a bit, then check again
 
     sleep 10;
@@ -20,13 +24,13 @@ while [ $IS_ONLINE -eq 0 ]; do
 
     CHECKS=$[ $CHECKS + 1 ]
     if [ $CHECKS -gt $MAX_CHECKS ]; then
-		sleep 300;
         break
     fi
 done
 
-if [ $IS_ONLINE -eq 0 ]; then
+if [[ $IS_ONLINE -eq 0 ]]; then
     # We never were able to get online. Kill script.
+	echo "Didn't get online Status" > /home/pi/klipper_config/script/gitpush.log
     exit 1
 fi
 
